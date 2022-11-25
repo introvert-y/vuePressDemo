0d68e06671bc0a547373c202b3c2ecb285243d7c
y <m15577822957@163.com>
2022-11-25 21:36:10 +0800

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:introvert-y/introvert-y.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:introvert-y/vuePressDemo.git master:gh-pages
 
 cd -
